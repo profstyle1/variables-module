4dcdd305b743b6c27314d5fbcc2ba231ad00a732
Ryan Thompson <service@aiwebsystems.com>
2018-04-16 07:20:20 -0500

    Override'

--- src/Variable/Form/VariableFormBuilder.php
+++ src/Variable/Form/VariableFormBuilder.php
@@ -1,6 +1,8 @@
 <?php namespace Anomaly\VariablesModule\Variable\Form;
 
+use Anomaly\Streams\Platform\Entry\EntryModel;
 use Anomaly\Streams\Platform\Ui\Form\FormBuilder;
+use Illuminate\Contracts\Container\Container;
 
 /**
  * Class VariableFormBuilder
@@ -13,16 +15,27 @@ class VariableFormBuilder extends FormBuilder
 {
 
     /**
-     * The form buttons.
+     * Fired just before building.
      *
-     * @var array
+     * @param Container $container
      */
-    protected $buttons = [
-        'versions' => [
-            'href' => 'admin/variables/versions/{entry.id}?group={request.route.parameters.id}',
-        ],
-        'cancel',
-    ];
+    public function onReady(Container $container)
+    {
+
+        /* @var EntryModel $model */
+        $model = $container->make($this->getModel());
+
+        if ($model->isVersionable()) {
+            $this->setButtons(
+                [
+                    'versions' => [
+                        'href' => 'admin/variables/versions/{entry.id}?group={request.route.parameters.id}',
+                    ],
+                    'cancel',
+                ]
+            );
+        }
+    }
 
     /**
      * Get the contextual entry ID.
